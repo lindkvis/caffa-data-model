cac0fc0c0e10eec80529709fb906a322c7debba5
Gaute Lindkvist <glindkvist@kontur.tech>
2024-09-18 13:04:28 +0200

    Replace DefaultObjectFactory::classes() method with a more modern version

--- cafDefaultObjectFactory.cpp
+++ cafDefaultObjectFactory.cpp
@@ -4,19 +4,6 @@
 
 namespace caffa
 {
-//--------------------------------------------------------------------------------------------------
-///  ObjectFactory implementations
-//--------------------------------------------------------------------------------------------------
-
-std::list<std::string> DefaultObjectFactory::classes() const
-{
-    std::list<std::string> classList;
-    for ( const auto& name : std::views::keys( m_factoryMap ) )
-    {
-        classList.push_back( name );
-    }
-    return classList;
-}
 
 //--------------------------------------------------------------------------------------------------
 ///
